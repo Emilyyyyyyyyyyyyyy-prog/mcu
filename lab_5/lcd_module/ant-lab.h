be9e5b3b23a895d408325d1186b600c6e5908adb
Emilyyyyyyyyyyyyyy-prog <amaliahtsvetkova@yandex.ru>
2025-03-13 03:15:46 +0300

    5lab

---
+++ lab_5/lcd_module/ant-lab.h
@@ -0,0 +1,4 @@
+#pragma once
+
+uint8_t* get_antlab_image_buffer(void);
+void draw_antlab(uint8_t* buffer);
---
+++ lab_5/lcd_module/gradient.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+
+void make_gradient(uint8_t* buffer);
+
+uint16_t colour(uint8_t R, uint8_t G, uint8_t B);
